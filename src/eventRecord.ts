e5e60465dff76baf86935f20d48dee28ca02bbdb
NyelDuncan110 <larsriver@yahoo.com>
2019-11-01 18:11:00 -0700

    event class test

---
+++ src/eventRecord.ts
@@ -0,0 +1,19 @@
+/*
+  Main Event Class
+  Larson Rivera
+*/
+
+class EventRecord {
+
+  constructor(n: String, p: number) { // Constructor to create a named event with the number of points it needs to add
+    this.name = n;
+    this.pts = p;
+  }
+
+  addPointsRecursive(ls: list){
+
+  }
+
+
+
+}
