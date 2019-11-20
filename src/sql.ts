a24d7189c913c4184526f03de9b06993645d4af6
not_a_seagull <notaseagull048@gmail.com>
2019-11-20 12:54:25 -0800

    Added BSD license boilerplate to all of the source files

--- src/sql.ts
+++ src/sql.ts
@@ -1,10 +1,41 @@
-// BSD LICENSE - c John Nunley and Larson Rivera
+/*
+ * src/sql.ts
+ * LeadershipTrakcer - Martial arts attendance logger
+ *
+ * Copyright (c) 2019, John Nunley and Larson Rivera
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this
+ *    list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * 3. Neither the name of the copyright holder nor the names of its
+ *    contributors may be used to endorse or promote products derived from
+ *    this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
 
 // sql statements
-import { Pool } from 'pg';
-import * as path from 'path';
+import { Pool } from "pg";
+import * as path from "path";
 
-const config = require(path.join(process.cwd(), 'config.json'));
+const config = require(path.join(process.cwd(), "config.json"));
 
 const pool = new Pool({
   user: config.postgres_username,
--- test/callbackify.js
+++
@@ -1,3 +0,0 @@
-// BSD LICENSE - c John Nunley and Larson Rivera
-
-// mocha is dumb and does not support promises
